084018840a7c016deaf0467cad1c88bad236dd0f
GriFFen75 <97539846+GriFFen75@users.noreply.github.com>
2022-04-22 17:47:40 +0200

    videoreader

--- src/apiwiwi.java
+++ src/apiwiwi.java
@@ -7,7 +7,8 @@ import java.net.*;
 public class apiwiwi {
     public static void main(String [] args) throws IOException {
         //https://api.themoviedb.org/3/movie/550?api_key=f3d7678cf05f1b8ce1d237f960b98786
-        URL url = new URL("https://api.themoviedb.org/3/movie/551?api_key=f3d7678cf05f1b8ce1d237f960b98786");
+        //https://api.themoviedb.org/3/search/movie?api_key=f3d7678cf05f1b8ce1d237f960b98786&query=
+        URL url = new URL("https://api.themoviedb.org/3/search/movie?api_key=f3d7678cf05f1b8ce1d237f960b98786&language=fr&query=avenger");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setDoOutput(true);
