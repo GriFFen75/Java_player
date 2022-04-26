11284d45a79f412ebd9e97af4120f01877fbd01e
Knupwx <wx.perso@gmail.com>
2022-04-26 20:28:58 +0200

    push wiwi

--- src/apiwiwi.java
+++ src/apiwiwi.java
@@ -3,16 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class apiwiwi {
-    public static void main(String [] args) throws IOException {
+
+    public static String Searchwiwi(String film, String data) throws IOException {
         //https://api.themoviedb.org/3/movie/550?api_key=f3d7678cf05f1b8ce1d237f960b98786
         //https://api.themoviedb.org/3/search/movie?api_key=f3d7678cf05f1b8ce1d237f960b98786?language=fr&query=the+avengers
         //https://www.themoviedb.org/search?language=fr&query=harry
         //1d66ea43
         //http://img.omdbapi.com/?apikey=1d66ea43
         //http://www.omdbapi.com/?apikey=1d66ea43&t=orphan+black&plot=full
-        URL url = new URL("http://www.omdbapi.com/?t=orphan+black&apikey=1d66ea43&plot=short");
+        URL url = new URL("http://www.omdbapi.com/?apikey=1d66ea43&plot=short&t="+film);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setDoOutput(true);
@@ -22,18 +25,41 @@ public class apiwiwi {
         BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
 
         String output;
-        System.out.println("Output from Server .... \n");
+        List<String> apifilm = new ArrayList<>();
         while ((output = br.readLine()) != null) {
+            System.out.println(output);
+            int a = -1;
+            String temp = "";
             //System.out.println(output);
 
             for(int i = 0 ; i < output.length();i++){
-                if(output.charAt(i)==','){
-                    System.out.println();
+
+                if(output.charAt(i)=='"'){
+                    a++;
                 }
-                else {
-                    System.out.print(output.charAt(i));
+                if(a==4) {
+                    //System.out.println();
+                    apifilm.add(temp);
+                    temp = "";
+                    a=0;
+                }
+                if(output.charAt(i)=='['){
+                    a=-5;
+                }
+                if(output.charAt(i)!=(',') & output.charAt(i)!='[' & output.charAt(i)!=']' & output.charAt(i)!='{' & output.charAt(i)!='}')
+                    temp += output.charAt(i);
+            }
+            for (Object o : apifilm) {
+                if(String.valueOf(o).contains(data)){
+                    String []temp2 = String.valueOf(o).split(":");
+                    return temp2[1].replaceAll("\"","");
                 }
             }
         }
+        return "mal ecrit?";
+    }
+    public static void main(String [] args) throws IOException {
+        System.out.println(Searchwiwi("alice".replace(" ","+"),"Title"));
+
     }
 }
