59c7e075a2a78caf8687c05090f85b8062002e0e
Knupwx <wx.perso@gmail.com>
2022-04-26 19:25:34 +0200

    push wiwi

--- src/apiwiwi.java
+++ src/apiwiwi.java
@@ -9,7 +9,10 @@ public class apiwiwi {
         //https://api.themoviedb.org/3/movie/550?api_key=f3d7678cf05f1b8ce1d237f960b98786
         //https://api.themoviedb.org/3/search/movie?api_key=f3d7678cf05f1b8ce1d237f960b98786?language=fr&query=the+avengers
         //https://www.themoviedb.org/search?language=fr&query=harry
-        URL url = new URL("https://api.themoviedb.org/3/search/movie?api_key=f3d7678cf05f1b8ce1d237f960b98786&language=fr&query=oprhan+black");
+        //1d66ea43
+        //http://img.omdbapi.com/?apikey=1d66ea43
+        //http://www.omdbapi.com/?apikey=1d66ea43&t=orphan+black&plot=full
+        URL url = new URL("http://www.omdbapi.com/?t=orphan+black&apikey=1d66ea43&plot=short");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         con.setDoOutput(true);
@@ -22,8 +25,9 @@ public class apiwiwi {
         System.out.println("Output from Server .... \n");
         while ((output = br.readLine()) != null) {
             //System.out.println(output);
+
             for(int i = 0 ; i < output.length();i++){
-                if(output.charAt(i)=='{'){
+                if(output.charAt(i)==','){
                     System.out.println();
                 }
                 else {
