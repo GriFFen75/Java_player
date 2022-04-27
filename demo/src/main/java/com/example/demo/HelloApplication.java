55fb02a1c03f44d694b46287bf86bfc50d1a6120
GriFFen75 <97539846+GriFFen75@users.noreply.github.com>
2022-04-27 14:50:20 +0200

    lecture de video 3(mais probleme)

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -1,31 +1,29 @@
 package com.example.demo;
 
-import java.awt.*;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
-import javafx.embed.swing.JFXPanel;
-
 import javax.swing.*;
-import javax.swing.border.Border;
+import java.awt.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class HelloApplication {
 
 
     public static String file ;
     public static JFrame frame = new JFrame("BVW / Lecteur vidéos");
-    public static JFXPanel fxPanel;
     public static JPanel panelVideo;
+    public static JPanel panelFx;
     public static Image icon = Toolkit.getDefaultToolkit().getImage("image/BVW.png");
 
     private static void initAndShowGUI() {
@@ -39,20 +37,25 @@ public class HelloApplication {
         frame.setIconImage(icon);
         createPanel();
     }
-    public static void createPanel(){
+    private static void createPanel(){
         //créer un panel pour réunir la video + la barre avec les boutons pour la modif de la vidéo
         panelVideo = new JPanel(new BorderLayout());
-        //on met de border pour que ca sois plus joli
+        //on met de border pour que ça sois plus joli
         panelVideo.setBorder(BorderFactory.createLineBorder(Color.white, 1));
-        panelVideo.setBackground(Color.BLACK);
+        //panelVideo.setBackground(Color.BLACK);
 
-        fxPanel = new JFXPanel();
-        fxPanel.setPreferredSize(new Dimension(800,600));
-        panelVideo.add(fxPanel,BorderLayout.CENTER);
+        JFXPanel fxPanel = new JFXPanel();
+        //fxPanel.setPreferredSize(new Dimension(800,600));
+
+        panelFx = new JPanel();
+        panelFx.add(fxPanel);
+
+        panelVideo.add(panelFx,BorderLayout.CENTER);
 
         //panel où il y aura tous les boutons
         JPanel panelOption = new JPanel();
-        panelOption.setBackground(Color.BLACK);
+        //panelOption.setBackground(Color.BLACK);
+        panelOption.setOpaque(false);
         JButton boutonPlay = new JButton("Play");
         boutonPlay.setIcon(new ImageIcon("image/play.png"));
         boutonPlay.addActionListener(e -> actionPlay());
@@ -64,7 +67,10 @@ public class HelloApplication {
         boutonStop.addActionListener(e -> actionStop());
         JButton fullScreen = new JButton();
         fullScreen.setIcon(new ImageIcon("image/fullScreen.png"));
-        fullScreen.addActionListener(e -> fenetreFullScreen());
+        fullScreen.addActionListener(e -> {
+            fenetreFullScreen();
+            close(frame);
+        });
         panelOption.add(boutonPlay);
         panelOption.add(boutonPause);
         panelOption.add(boutonStop);
@@ -92,23 +98,32 @@ public class HelloApplication {
         frame.add(panelInfo,BorderLayout.WEST);
 
         frame.setVisible(true);
-        initFX();
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+        });
 
     }
-    JFrame frameVideo ;
-    public static void fenetreFullScreen (){
+    private static void fenetreFullScreen (){
         JFrame frameVideo = new JFrame("BVW / Lecture");
         frameVideo.setLayout(new BorderLayout());
         frameVideo.setIconImage(icon);
         frameVideo.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frameVideo.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frameVideo.setUndecorated(true); //pour enlever la barre windows en haut
-        frameVideo.add(fxPanel);
+        //frameVideo.setUndecorated(true); //pour enlever la barre windows en haut
+
+        JFXPanel fxPanel = new JFXPanel();
+        frameVideo.add(fxPanel,BorderLayout.CENTER);
         frameVideo.setVisible(true);
 
         //panel où il y aura tous les boutons
         JPanel panelOptionFullScreen = new JPanel();
-        panelOptionFullScreen.setBackground(Color.BLACK);
+        //panelOptionFullScreen.setBackground(Color.BLACK);
+        panelOptionFullScreen.setOpaque(false);
+        //panelOptionFullScreen.setBackground(Color.BITMASK);
         JButton boutonPlay = new JButton("Play");
         boutonPlay.setIcon(new ImageIcon("image/play.png"));
         boutonPlay.addActionListener(e -> actionPlay());
@@ -120,35 +135,47 @@ public class HelloApplication {
         boutonStop.addActionListener(e -> actionStop());
         JButton fullScreen = new JButton();
         fullScreen.setIcon(new ImageIcon("image/fullScreen.png"));
-        fullScreen.addActionListener(e -> close(frameVideo));
+        fullScreen.addActionListener(e -> {
+            close(frameVideo);
+            main(null,UpdatePath());
+        });
         panelOptionFullScreen.add(boutonPlay);
         panelOptionFullScreen.add(boutonPause);
         panelOptionFullScreen.add(boutonStop);
         panelOptionFullScreen.add(fullScreen);
-        panelVideo.add(panelOptionFullScreen, BorderLayout.SOUTH);
+        frameVideo.add(panelOptionFullScreen, BorderLayout.SOUTH);
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                initFX();
+                initFX(fxPanel);
             }
         });
+    }
+    private static void actionPlay(){
+        mediaPlayer.play();
     }
 
     public static void closeFrame(){ // parce que j'ai 2 fenêtres qui s'ouvrent
         frame.dispose();
     }
-    public static void close(JFrame frame){
+    private static void close(JFrame frame){
         frame.dispose();
     }
-    public static MediaPlayer mediaPlayer;
-    private static void initFX() {
 
-        Media media = new Media(new File(UpdatePath()).toURI().toString());
+    private static MediaPlayer mediaPlayer;
+
+    private static void initFX(JFXPanel fxPanel) {
+
+        Media media = null;
+        System.out.println(media);
+        media = new Media(new File(UpdatePath()).toURI().toString());
         System.out.println(media);
         mediaPlayer = new MediaPlayer(media);
         System.out.println(media);
         mediaPlayer.setAutoPlay(false);
+        System.out.println(media);
         MediaView mediaView = new MediaView(mediaPlayer);
+        System.out.println(media);
 
         Group root = new Group();
         root.getChildren().add(mediaView);
@@ -157,25 +184,25 @@ public class HelloApplication {
         mvw.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
         mvh.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
         mediaView.setPreserveRatio(true);
+        System.out.println(media);
 
 
         //Scene scene = new Scene(root,1000,800);
         fxPanel.setScene(new Scene(new Group(mediaView), panelVideo.getWidth(), panelVideo.getHeight()));
     }
-    public static void actionPlay(){
-        mediaPlayer.play();
-    }
-    public static void actionStop(){
+
+    private static void actionStop(){
         mediaPlayer.stop();
     }
-    public static void actionPause(){
+    private static void actionPause(){
         mediaPlayer.pause();
     }
 
     public static String UpdatePath(){
         Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
-        String path = new String(s+"/video/"+file);
+        String path = "";
+        path = (s+"/video/"+file);
         return path;
     }
 
